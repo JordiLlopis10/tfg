d2473d051609b0f96ba34a3ab9c5e04b7e99566a
JordiLlopis10 <llopisgodinojordi@gmail.com>
2025-06-11 17:33:05 +0200

    añadido cludfare

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -59,12 +59,11 @@
     <footer class="footer">
       <div class="footer-content">
         <p>© 2025 Detalls de Patch. Todos los derechos reservados.</p>
-        <div class="social-icons">
+        <div class="redes-sociales">
           <a href="https://www.youtube.com/@detallspatch" target="_blank"><i class="fab fa-youtube"></i></a>
           <a href="https://www.instagram.com/detallspatch" target="_blank"><i class="fab fa-instagram"></i></a>
           <a href="https://www.tiktok.com/@detallspatch" target="_blank"><i class="fab fa-tiktok"></i></a>
           <a href="https://www.facebook.com/detallspatch" target="_blank"><i class="fab fa-facebook-f"></i></a>
-          <p>Yeee</p>
         </div>
       </div>
     </footer>
@@ -74,6 +73,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
 
 const noticia = ref('')
 const destacados = ref([])
@@ -351,5 +351,20 @@ html, body {
   margin: 0 auto;
 }
 
+.redes-sociales {
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  margin-top: 1rem;
+}
 
+.redes-sociales a {
+  color: #842c56; 
+  font-size: 1.5rem;
+  transition: color 0.2s;
+}
+
+.redes-sociales a:hover {
+  color: #eac6c6; 
+}
 </style>
