a51dbdb367348f3ee241160c9ce199e9498cb48f
JordiLlopis10 <llopisgodinojordi@gmail.com>
2025-06-11 13:40:42 +0200

    cors

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,8 @@ app.config["MYSQL_SSL_VERIFY_CERT"] = True
 
 mysql = MySQL(app)
 
-CORS(app, resources={r"/*": {"origins": "https://detallspatch.onrender.com/"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "https://detallspatch.vercel.app"}}, supports_credentials=True)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
